1d3771c1a38cfd2e7e01c6395aaf5cae1414fc4f
Srinija kanuganti <srinijakanuganti2006@gmail.com>
2026-08-19 16:11:31 +0530

    Add files via upload

---
+++ stringify.js
@@ -0,0 +1,8 @@
+var accountObj ={
+    name:"SRINIJA",
+    number:10645,
+    members:["POOJA,ANVI"],
+    location:"WARANGAL"
+};
+var accountStr = JSON.stringify(accountObj);
+console.log(accountStr);
